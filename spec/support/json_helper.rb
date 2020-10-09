2ff9aaa31817b0827254ed79f9fb7653bb79bb0b
Fabiano Leite <fabianoleittes@gmail.com>
2020-10-09 02:23:39 -0300

    feat: add json helper for test

---
+++ spec/support/json_helper.rb
@@ -0,0 +1,26 @@
+module JsonHelper
+  def json
+    (reparse_and_never_memoize_as_response_may_change = -> do
+      JSON.parse(response.body)
+    end).call
+  end
+
+  def headers
+    {
+      'Authorization' => token_generator(user.id),
+      'Content-Type' => 'application/json'
+    }
+  end
+
+  def user
+    user ||= create(:user)
+  end
+
+  def token_generator(user_id, adapter = JsonWebTokenAdapter)
+    adapter.encode(user_id: user_id)
+  end
+end
+
+RSpec.configure do |config|
+  config.include JsonHelper, type: :request
+end
