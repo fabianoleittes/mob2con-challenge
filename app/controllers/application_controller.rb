aefaa4a6540d4825602f51959a594d135288545d
Fabiano Leite <fabianoleittes@gmail.com>
2020-10-11 16:57:12 -0300

    feat: create namespace admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+
+  attr_reader :current_user
+
+  private
+
+  def authorize_request
+    @current_user = Authorize.call(request.headers)
+  end
 end
--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -2,23 +2,5 @@
 
 module V1
   class BaseController < ApplicationController
-    include Response
-    include ExceptionHandler
-
-    attr_reader :current_user
-
-    private
-
-    def authorize_request
-      @current_user = Authorize.call(request.headers)
-    end
-
-    def logged_as_admin?
-      current_user.admin?
-    end
-
-    def require_be_admin
-      raise NotPermittedException unless logged_as_admin?
-    end
   end
 end
