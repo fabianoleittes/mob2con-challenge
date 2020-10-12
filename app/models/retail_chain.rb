ec1d755b77f7ff59891ccbff7c3acf6408ce447e
Fabiano Leite <fabianoleittes@gmail.com>
2020-10-11 21:29:06 -0300

    feat: search retail chains by name

--- app/models/retail_chain.rb
+++ app/models/retail_chain.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 class RetailChain < ApplicationRecord
   validates :name, presence: true
   validates :cnpj, presence: true, uniqueness: { case_sensitive: false }
 
   has_many :users, dependent: :nullify
+
+  scope :search_by_name, lambda { |params|
+    return unless params.key?(:name)
+
+    where('name LIKE ?', params[:name])
+  }
 end
